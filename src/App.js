14567236897b2ec774f16ef13f7db716ee3fbd72
Marco Richetta <marcorichetta@gmail.com>
2019-10-07 22:16:39 -0300

    6.7 - New component for Anecdote form

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import { createAnecdote } from './reducers/anecdoteReducer';
 import Anecdotes from './components/Anecdotes';
+import AnecdoteForm from './components/AnecdoteForm';
 
 const App = (props) => {
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-
-    props.store.dispatch(
-      createAnecdote(content)
-    )
-  }
 
   return (
     <div>
       <h2>Anecdotes</h2>
       <Anecdotes store={props.store} />
       <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div>
-          <input name="anecdote" />
-        </div>
-        <button>create</button>
-      </form>
+      <AnecdoteForm store={props.store} />
     </div>
   )
 }
